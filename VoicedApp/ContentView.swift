c409ee2bf11908657ea55eb849af617c81dd450e
Abraham Morales Arroyo <abrahamof606@gmail.com>
2024-03-08 10:23:15 -0600

    Initial Commit

---
+++ VoicedApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VoicedApp
+//
+//  Created by Abraham Morales Arroyo on 3/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VoicedApp/VoicedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoicedAppApp.swift
+//  VoicedApp
+//
+//  Created by Abraham Morales Arroyo on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct VoicedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
