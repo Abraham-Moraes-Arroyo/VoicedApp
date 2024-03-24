b9a38e09111af4ce7bc524402c380a38f73a339c
joannarodriguez134 <joannarodriguez134@gmail.com>
2024-03-24 16:06:23 -0500

    Uploading a post data fields now visible on firebase

--- VoicedApp/Core/PostCreation/Model/UploadPostViewModel.swift
+++ VoicedApp/Core/PostCreation/Model/UploadPostViewModel.swift
@@ -5,6 +5,7 @@
 //  Created by Joanna Rodriguez on 3/19/24.
 //
 
+import Foundation
 import SwiftUI
 import PhotosUI
 import Firebase
@@ -14,68 +15,36 @@ class UploadPostViewModel: ObservableObject {
     @Published var selectedImage: PhotosPickerItem? {
         didSet { Task { await loadImage(fromItem: selectedImage) } }
     }
-    @Published var postImage: Image?
     
+    @Published var postImage: Image?
     private var uiImage: UIImage?
-    private let defaultImage = Image("default-post-image") // Local default image
-    private let defaultImageUrl = "https://firebasestorage.googleapis.com/v0/b/voiced-b1f36.appspot.com/o/profile_images%2Fdefault-post-image.jpg?alt=media&token=99f51562-b411-49b6-92ef-abbf00af15aa"
-
-    init() {
-        // Initialize with the default image
-        self.postImage = defaultImage
-    }
+    
+//     this function is looking at the item from the photospicker item ->
+//   creates data -> creates uiImage -> which generates the swiftUI image
     
     func loadImage(fromItem item: PhotosPickerItem?) async {
-        guard let item = item,
-              let data = try? await item.loadTransferable(type: Data.self),
-              let uiImage = UIImage(data: data) else {
-            // If no image is selected or fails to load, revert to default image
-            self.postImage = defaultImage
-            return
-        }
+        guard let item = item else { return }
         
+        guard let data = try? await item.loadTransferable(type: Data.self) else { return }
+        
+        guard let uiImage = UIImage(data: data) else { return }
         self.uiImage = uiImage
         self.postImage = Image(uiImage: uiImage)
     }
     
-    func uploadPost(title: String, caption: String = "", category: PostCategory) async throws {
-        guard !title.isEmpty else {
-            // Title is required
-            throw UploadError.missingTitle
-        }
+    func uploadPost(title: String, caption: String, category: PostCategory) async throws {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        guard let uiImage = uiImage else { return }
         
-        guard let uid = Auth.auth().currentUser?.uid else {
-            throw UploadError.unauthorized
-        }
+        // create post collection:
+        let postRef = Firestore.firestore().collection("posts").document()
         
-        // Decide the imageUrl based on if a custom image was selected
-        let imageUrl = uiImage != nil ? await uploadCustomImage() : defaultImageUrl
-    
-        
-        // Create and upload the post
-        let post = Post(
-            id: UUID().uuidString,
-            ownerUid: uid,
-            title: title,
-            caption: caption,
-            likes: 0,
-            dislikes: 0,
-            imageUrl: imageUrl,
-            timestamp: Timestamp(),
-            category: category
-        )
+        guard let imageUrl = try await ImageUploader.uploadImage(image: uiImage) else { return }
+
+        let post = Post(id: postRef.documentID, ownerUid: uid, title: title, caption: caption, likes: 0, dislikes: 0, imageUrl: imageUrl, timestamp: Timestamp(), category: PostCategory(rawValue: category.rawValue) ?? PostCategory.miscellaneous)
         
         guard let encodedPost = try? Firestore.Encoder().encode(post) else { return }
-        let postRef = Firestore.firestore().collection("posts").document(post.id)
+        // upload post to firestore
         try await postRef.setData(encodedPost)
-    }
-    
-    private func uploadCustomImage() async -> String {
-        // existing image uploading logic, returning the URL of the uploaded image
-        return "default-post-image"
-    }
-    
-    enum UploadError: Error {
-        case missingTitle, unauthorized, encodingFailed
     }
 }
