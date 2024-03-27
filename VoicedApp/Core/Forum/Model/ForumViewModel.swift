cf43a20f693f7d44728e100926cd338e0c324ef5
joannarodriguez134 <joannarodriguez134@gmail.com>
2024-03-26 22:01:29 -0500

    added filtering by posts category in forum view

--- VoicedApp/Core/Forum/Model/ForumViewModel.swift
+++ VoicedApp/Core/Forum/Model/ForumViewModel.swift
@@ -11,6 +11,9 @@ import Firebase
 // class responsible for fetching posts and displaying them
 class ForumViewModel: ObservableObject {
     @Published var posts = [Post]()
+    @Published var filteredPosts = [Post]()
+        private var currentCategory: PostCategory = .miscellaneous
+    // current category represents the default category
     
     init() {
         Task { try await fetchPosts() }
@@ -19,5 +22,22 @@ class ForumViewModel: ObservableObject {
     @MainActor
     func fetchPosts() async throws {
         self.posts = try await PostService.fetchForumPosts()
+        
+        applyCategoryFilter(category: currentCategory)
     }
-}
+    // Method to set the current category and filter posts accordingly
+        func setCategory(_ category: PostCategory) {
+            currentCategory = category
+            applyCategoryFilter(category: category)
+        }
+        
+        // Filter posts by the selected category
+        private func applyCategoryFilter(category: PostCategory) {
+            if category == .miscellaneous {
+                // default category
+                filteredPosts = posts
+            } else {
+                filteredPosts = posts.filter { $0.category == category }
+            }
+        }
+    }
--- VoicedApp/Core/Forum/Views/ForumView.swift
+++ VoicedApp/Core/Forum/Views/ForumView.swift
@@ -10,23 +10,17 @@ import SwiftUI
 struct ForumView: View {
     @StateObject var viewModel = ForumViewModel()
     @State private var isProfileViewActive = false
-    @State private var sortingOption = "Miscellaneous"
-
-    // This computed property filters the MOCK_POSTS to include only user-generated posts.
-    var userGeneratedPosts: [Post] {
-        viewModel.posts.filter { $0.isUserGenerated }
-    }
+    @State private var selectedCategory: PostCategory = .miscellaneous // Updated to use PostCategory enum
 
     var body: some View {
         NavigationStack {
             ScrollView(showsIndicators: false) {
                 LazyVStack(spacing: 35) {
-                    // Use ForEach to dynamically create ForumCell views for each filtered user-generated post
-                    ForEach(userGeneratedPosts, id: \.id) { post in
+                    // Dynamically create ForumCell views for each post in filteredPosts
+                    ForEach(viewModel.filteredPosts, id: \.id) { post in
                         ForumCell(post: post)
                             .onTapGesture {
                                 // Placeholder for future implementation
-                                // Will need to set a state variable here to determine which profile to show
                                 isProfileViewActive = true
                             }
                     }
@@ -53,22 +47,21 @@ struct ForumView: View {
                 
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Menu {
-                        Button("Miscellaneous", action: { sortingOption = "Miscellaneous" })
-                        Button("Reports", action: { sortingOption = "Reports" })
-                        Button("Happy Highlights", action: { sortingOption = "Happy Highlights" })
-                        Button("Events", action: { sortingOption = "Events" })
-                        Button("Business", action: { sortingOption = "Business" })
+                        ForEach(PostCategory.allCases, id: \.self) { category in
+                            Button(category.displayName, action: {
+                                selectedCategory = category
+                                viewModel.setCategory(category)
+                            })
+                        }
                     } label: {
                         Image(systemName: "ellipsis")
                     }
                 }
             }
             .foregroundColor(.black)
-            
-            // Keep NavigationLink for later logic implementation
-//            NavigationLink(destination: ProfileView(viewModel: <#ProfileViewModel#>, user: User.MOCK_USERS[0])) {
-//                EmptyView()
-//            }
+        }
+        .onChange(of: selectedCategory) { newValue in
+            viewModel.setCategory(newValue)
         }
     }
 }
