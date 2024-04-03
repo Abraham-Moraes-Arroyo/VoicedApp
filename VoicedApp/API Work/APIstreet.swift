2cac46f1a3190c886775d20745f7aea6bd24766a
Abraham-Moraes-Arroyo <77249193+Abraham-Moraes-Arroyo@users.noreply.github.com>
2024-04-03 10:20:45 -0500

    Started to add the json files for the API Call links.

---
+++ VoicedApp/API Work/APIstreet.swift
@@ -0,0 +1,20 @@
+//
+//  APIstreet.swift
+//  VoicedApp
+//
+//  Created by Abraham Morales Arroyo on 4/3/24.
+//
+
+//https://www.youtube.com/watch?v=bgznym00FyY
+
+import SwiftUI
+
+struct APIstreet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    APIstreet()
+}
