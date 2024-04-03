bf0a7803d5459bca398d0e45c67298188f3a894b
Abraham-Moraes-Arroyo <77249193+Abraham-Moraes-Arroyo@users.noreply.github.com>
2024-04-02 20:43:11 -0500

    Finished attempting, hopefull more can be done for tommorow

--- VoicedApp/API Work/DataPullJson.swift
+++ VoicedApp/API Work/DataPullJson.swift
@@ -6,36 +6,16 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct DataPullJson: View {
     
-    func getData(from url: String){
-       let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
-           guard let data = data, error == nil else {
-               print("something went wrong ")
-               return
-           }
-           // have data
-           // convert the data here.
-           var result: Response?
-           do{
-               result = try JSONDecoder().decode(Response.self, from: data)
-           } catch {
-               print("failed to convert \(error.localizedDescription)")
-           }
-           guard let json = result else {
-               return
-           }
-           // we print out some of the values
-           print(json.results.SR_TYPE)
-           print(json.results.Community_Area)
-           print(json.results.status)
-       })
-       task.resume()
-    }
+    
 
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        UIViewController
     }
 }
 
@@ -43,3 +23,30 @@ struct DataPullJson: View {
     DataPullJson()
 }
 
+
+extension UIViewController{
+    func getData(from url: String){
+      let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
+          guard let data = data, error == nil else {
+              print("something went wrong ")
+              return
+          }
+          // have data
+          // convert the data here.
+          var result: Response?
+          do{
+              result = try JSONDecoder().decode(Response.self, from: data)
+          } catch {
+              print("failed to convert \(error.localizedDescription)")
+          }
+          guard let json = result else {
+              return
+          }
+          // we print out some of the values
+          print(json.results.SR_TYPE)
+          print(json.results.Community_Area)
+          print(json.results.status)
+      })
+      task.resume()
+   }
+}
