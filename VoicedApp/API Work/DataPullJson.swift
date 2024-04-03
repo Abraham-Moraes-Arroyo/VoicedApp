94adbbe0c09286492a740eda6ebf3ecd2d142718
Abraham-Moraes-Arroyo <77249193+Abraham-Moraes-Arroyo@users.noreply.github.com>
2024-04-03 11:41:35 -0500

    tried to get the goblal variable to work.

--- VoicedApp/API Work/DataPullJson.swift
+++ VoicedApp/API Work/DataPullJson.swift
@@ -7,46 +7,48 @@
 
 import SwiftUI
 import Foundation
-//
-//struct DataPullJson: View {
-//    
-//    
-//
-//    var body: some View {
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-//        
-//        UIViewController
-//    }
-//}
-//
-//#Preview {
-//    DataPullJson()
-//}
-//
-//
-//extension UIViewController{
-//    func getData(from url: String){
-//      let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
-//          guard let data = data, error == nil else {
-//              print("something went wrong ")
-//              return
-//          }
-//          // have data
-//          // convert the data here.
-//          var result: Response?
-//          do{
-//              result = try JSONDecoder().decode(Response.self, from: data)
-//          } catch {
-//              print("failed to convert \(error.localizedDescription)")
-//          }
-//          guard let json = result else {
-//              return
-//          }
-//          // we print out some of the values
-//          print(json.results.SR_TYPE)
-//          print(json.results.Community_Area)
-//          print(json.results.status)
-//      })
-//      task.resume()
-//   }
-//}
+
+struct DataPullJson: View {
+    
+    
+
+    var body: some View {
+        VStack{
+            Text("Hello, World!")
+            
+            getanddisplay
+        }
+    }
+}
+
+#Preview {
+    DataPullJson()
+}
+// based this solution on https://stackoverflow.com/questions/27050580/how-are-global-functions-defined-in-swift
+
+extension getanddisplay{
+    func getData(from url: String){
+      let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
+          guard let data = data, error == nil else {
+              print("something went wrong ")
+              return
+          }
+          // have data
+          // convert the data here.
+          var result: Response?
+          do{
+              result = try JSONDecoder().decode(Response.self, from: data)
+          } catch {
+              print("failed to convert \(error.localizedDescription)")
+          }
+          guard let json = result else {
+              return
+          }
+          // we print out some of the values
+          print(json.results.SR_TYPE)
+          print(json.results.Community_Area)
+          print(json.results.status)
+      })
+      task.resume()
+   }
+}
