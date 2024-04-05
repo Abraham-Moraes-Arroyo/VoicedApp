47e2a77491f53a9c891f58cfec5499a0af556e3e
Abraham-Moraes-Arroyo <77249193+Abraham-Moraes-Arroyo@users.noreply.github.com>
2024-04-05 07:24:42 -0500

    Had to clean out some of the code that was no longer useful.

--- VoicedApp/API Pull Screens/SanitationCombined.swift
+++ VoicedApp/API Pull Screens/SanitationCombined.swift
@@ -9,18 +9,18 @@ import SwiftUI
 import Charts
 
 
-// Sanitation Completed
-struct CompletedSanitation: Decodable, Identifiable {
-    let id = UUID()
-    let srNumber: String
-    let srType: String
-    let status: String
-    
-    // Use CodingKeys to map JSON keys to struct properties
-    private enum CodingKeys: String, CodingKey {
-        case srNumber = "sr_number", srType = "sr_type", status
-    }
-}
+//// Sanitation Completed
+//struct CompletedSanitation: Decodable, Identifiable {
+//    let id = UUID()
+//    let srNumber: String
+//    let srType: String
+//    let status: String
+//    
+//    // Use CodingKeys to map JSON keys to struct properties
+//    private enum CodingKeys: String, CodingKey {
+//        case srNumber = "sr_number", srType = "sr_type", status
+//    }
+//}
 
 // Sanitation Incomplete
 struct IncompleteSanitation: Decodable, Identifiable {
@@ -119,7 +119,7 @@ class SanitationStatusCompleteShown: ObservableObject {
         }.resume()
     }
     
-    // firest function
+    // first function
     func getOpenComplaintsCall() {
         let urlString = "https://data.cityofchicago.org/resource/v6vf-nfxy.json?sr_type=Sanitation%20Code%20Violation&&community_area=61&&status=Open"
         guard let url = URL(string: urlString) else {
@@ -191,85 +191,85 @@ class SanitationStatusCompleteShown: ObservableObject {
 // I am going to try to have both of the classes here for open and closed sanitation calles
 // WE ARE GOING TO BEGIN WITH THE COMPLETED CALLS
 // Define a ViewModel to fetch and store data from the API
-class FinishedSanitation: ObservableObject {
-    @Published var complaints2: [PotholeComplaint] = []
-    
-    // I just did a few fields in the Pothole url
-    // You will need to modify this code to grab any other stuff you need
-    func fetchData() {
-        let urlString = "https://data.cityofchicago.org/resource/v6vf-nfxy.json?sr_type=Sanitation%20Code%20Violation&&community_area=61&&status=Completed"
-        guard let url = URL(string: urlString) else {
-            print("Invalid URL")
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
-            if let data = data {
-                do {
-                    let decodedResponse = try JSONDecoder().decode([PotholeComplaint].self, from: data)
-                    DispatchQueue.main.async {
-                        self?.complaints2 = decodedResponse
-                    }
-                    // Print each complaint's details to the console for debugging.
-                    for complaint in decodedResponse {
-                        print("SR Number: \(complaint.srNumber), SR Type: \(complaint.srType), Status: \(complaint.status)")
-                    }
-                } catch {
-                    DispatchQueue.main.async {
-                        print("Decoding failed: \(error.localizedDescription)")
-                    }
-                }
-            } else if let error = error {
-                DispatchQueue.main.async {
-                    print("Fetch failed: \(error.localizedDescription)")
-                }
-            }
-        }.resume()
-    }
-}// End of completed Calls
-
+//class FinishedSanitation: ObservableObject {
+//    @Published var complaints2: [PotholeComplaint] = []
+//    
+//    // I just did a few fields in the Pothole url
+//    // You will need to modify this code to grab any other stuff you need
+//    func fetchData() {
+//        let urlString = "https://data.cityofchicago.org/resource/v6vf-nfxy.json?sr_type=Sanitation%20Code%20Violation&&community_area=61&&status=Completed"
+//        guard let url = URL(string: urlString) else {
+//            print("Invalid URL")
+//            return
+//        }
+//        
+//        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+//            if let data = data {
+//                do {
+//                    let decodedResponse = try JSONDecoder().decode([PotholeComplaint].self, from: data)
+//                    DispatchQueue.main.async {
+//                        self?.complaints2 = decodedResponse
+//                    }
+//                    // Print each complaint's details to the console for debugging.
+//                    for complaint in decodedResponse {
+//                        print("SR Number: \(complaint.srNumber), SR Type: \(complaint.srType), Status: \(complaint.status)")
+//                    }
+//                } catch {
+//                    DispatchQueue.main.async {
+//                        print("Decoding failed: \(error.localizedDescription)")
+//                    }
+//                }
+//            } else if let error = error {
+//                DispatchQueue.main.async {
+//                    print("Fetch failed: \(error.localizedDescription)")
+//                }
+//            }
+//        }.resume()
+//    }
+//}// End of completed Calls
+//
 
 // OPEN SANITATION CALLS
 // Define a ViewModel to fetch and store data from the API
-class OpenSantinationCalls: ObservableObject {
-    @Published var complaints: [PotholeComplaint] = []
-    
-    // I just did a few fields in the Pothole url
-    // You will need to modify this code to grab any other stuff you need
-    func fetchData() {
-        let urlString = "https://data.cityofchicago.org/resource/v6vf-nfxy.json?sr_type=Sanitation%20Code%20Violation&&community_area=61&&status=Open"
-        guard let url = URL(string: urlString) else {
-            print("Invalid URL")
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
-            if let data = data {
-                do {
-                    let decodedResponse = try JSONDecoder().decode([PotholeComplaint].self, from: data)
-                    DispatchQueue.main.async {
-                        self?.complaints = decodedResponse
-                    }
-                    // Print each complaint's details to the console for debugging.
-                    for complaint in decodedResponse {
-                        print("SR Number: \(complaint.srNumber), SR Type: \(complaint.srType), Status: \(complaint.status)")
-                    }
-                } catch {
-                    DispatchQueue.main.async {
-                        print("Decoding failed: \(error.localizedDescription)")
-                    }
-                }
-            } else if let error = error {
-                DispatchQueue.main.async {
-                    print("Fetch failed: \(error.localizedDescription)")
-                }
-            }
-        }.resume()
-    }
-}
-
-
-
+//class OpenSantinationCalls: ObservableObject {
+//    @Published var complaints: [PotholeComplaint] = []
+//    
+//    // I just did a few fields in the Pothole url
+//    // You will need to modify this code to grab any other stuff you need
+//    func fetchData() {
+//        let urlString = "https://data.cityofchicago.org/resource/v6vf-nfxy.json?sr_type=Sanitation%20Code%20Violation&&community_area=61&&status=Open"
+//        guard let url = URL(string: urlString) else {
+//            print("Invalid URL")
+//            return
+//        }
+//        
+//        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+//            if let data = data {
+//                do {
+//                    let decodedResponse = try JSONDecoder().decode([PotholeComplaint].self, from: data)
+//                    DispatchQueue.main.async {
+//                        self?.complaints = decodedResponse
+//                    }
+//                    // Print each complaint's details to the console for debugging.
+//                    for complaint in decodedResponse {
+//                        print("SR Number: \(complaint.srNumber), SR Type: \(complaint.srType), Status: \(complaint.status)")
+//                    }
+//                } catch {
+//                    DispatchQueue.main.async {
+//                        print("Decoding failed: \(error.localizedDescription)")
+//                    }
+//                }
+//            } else if let error = error {
+//                DispatchQueue.main.async {
+//                    print("Fetch failed: \(error.localizedDescription)")
+//                }
+//            }
+//        }.resume()
+//    }
+//}
+//
+//
+//
 
 
 
@@ -294,11 +294,12 @@ struct CombinedSanCalls: View { // make sure that this is differnet
                             y:.value("Completed Issues", viewModel.completedComplaints.count))
                     
                     
-                    BarMark(x:.value("type", "Open Pothole calls"),
-                            y:.value("Open Issues", viewModel.openComplaintPot.count))
-                    
                     BarMark(x:.value("type", "Closed Pothole calls"),
                             y:.value("Closed Issues", viewModel.closedComplaintPot.count))
+                    
+                    BarMark(x:.value("type", "Open Pothole calls"),
+                            y:.value("Open Issues", viewModel.openComplaintPot.count))
+                
                   
                     
 //                    BarMark(x:.value("Type", "Completed Pothole Number"),
